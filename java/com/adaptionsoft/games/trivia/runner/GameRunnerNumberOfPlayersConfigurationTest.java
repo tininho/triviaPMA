6d94126e816a3dc83f8c0c8adfea58a6da1abfff
Florentino Romero <fromeroharo@gmail.com>
2013-04-29 01:41:56 +0200

    eliminacion de test redundantes

--- java/com/adaptionsoft/games/trivia/runner/GameRunnerNumberOfPlayersConfigurationTest.java
+++ java/com/adaptionsoft/games/trivia/runner/GameRunnerNumberOfPlayersConfigurationTest.java
@@ -13,51 +13,6 @@ import com.adaptionsoft.games.util.Checker;
 public class GameRunnerNumberOfPlayersConfigurationTest {
 
 	@Test
-	public void gameRunnerCharacterizationSixPlayers() {
-		// given
-		Checker checker = new Checker();
-		System.setOut(new PrintStream(checker));
-		Game aGame = GameRunner.sixPlayersinit();
-		Random random = new Random(0L);
-
-		// check
-		GameRunner.run(aGame, random);
-
-		// verify
-		assertEquals(1834776121L, checker.getChecksumValue());
-	}
-
-	@Test(expected = Exception.class)
-	public void gameRunnerCharacterizationSevenPlayers() {
-		// given
-		Checker checker = new Checker();
-		System.setOut(new PrintStream(checker));
-		Game aGame = GameRunnerInspector.sevenPlayersInit();
-		Random random = new Random(0L);
-
-		// check
-		GameRunner.run(aGame, random);
-
-		// fail
-		fail("Exception not throw");
-	}
-
-	@Test
-	public void gameRunnerCharacterizationFourPlayers() {
-		// given
-		Checker checker = new Checker();
-		System.setOut(new PrintStream(checker));
-		Game aGame = GameRunnerInspector.fourPlayersInit();
-		Random random = new Random(0L);
-
-		// check
-		GameRunner.run(aGame, random);
-
-		// verify
-		assertEquals(1826728610L, checker.getChecksumValue());
-	}
-
-	@Test
 	public void gameRunnerCharacterizationOnlySixPlayers() {
 		// given
 		Checker checker = new Checker();
