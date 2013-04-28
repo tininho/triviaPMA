fafd7c741e376baf9ab38801d48c199c23653527
Florentino Romero <fromeroharo@gmail.com>
2013-04-29 01:33:16 +0200

    modificacion de cod produccion para contemplar la nueva funcionalidad

--- java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -7,14 +7,33 @@ import com.adaptionsoft.games.uglytrivia.Game;
 public class GameRunner {
 
 	private static boolean notAWinner;
-
+	
+	private static int DEFAULT_NUMBER_OF_PLAYERS = 6;
+	
+	private static String EMPTY_STRING = "";
+	
 	public static void main(String[] args) {
-		Game aGame = init();
+		int numberOfPlayers = DEFAULT_NUMBER_OF_PLAYERS;
+		
+		if (args[0]!=null && ! EMPTY_STRING.equals(args[0]))
+			numberOfPlayers = Integer.valueOf(args[0]);
+		
+		Game aGame = init(numberOfPlayers);
 
 		Random rand = new Random();
 
 		run(aGame, rand);
 
+	}
+	
+	static Game init(int numberOfPlayers) {
+		Game aGame = new Game(numberOfPlayers);
+		
+		for (int i = 0; i < numberOfPlayers; i++) {
+			aGame.add("Player "+(i+1));
+		}
+		
+		return aGame;
 	}
 
 	static Game init() {
--- java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -37,6 +37,26 @@ public class Game {
 			rockQuestions.addLast(createRockQuestion(i));
     	}
     }
+    
+    public Game(int numberOfPlayers) {
+
+		players = new ArrayList<String>(numberOfPlayers);
+		places = new int[numberOfPlayers];
+		purses = new int[numberOfPlayers];
+		inPenaltyBox = new boolean[numberOfPlayers];
+
+		popQuestions = new LinkedList();
+		scienceQuestions = new LinkedList();
+		sportsQuestions = new LinkedList();
+		rockQuestions = new LinkedList();
+
+		for (int i = 0; i < 50; i++) {
+			popQuestions.addLast("Pop Question " + i);
+			scienceQuestions.addLast(("Science Question " + i));
+			sportsQuestions.addLast(("Sports Question " + i));
+			rockQuestions.addLast(createRockQuestion(i));
+		}
+	}
 
 	public String createRockQuestion(int index){
 		return "Rock Question " + index;
