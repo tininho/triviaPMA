e09d7c2da835da355e0222c975b23a1adb3e46bb
Florentino Romero <fromeroharo@gmail.com>
2013-04-29 00:27:50 +0200

    error en funcionalidad de 6 jugadores

--- java/com/adaptionsoft/games/trivia/runner/GameRunnerNumberOfPlayersConfiguration.java
+++ java/com/adaptionsoft/games/trivia/runner/GameRunnerNumberOfPlayersConfiguration.java
@@ -13,16 +13,16 @@ import com.adaptionsoft.games.util.Checker;
 public class GameRunnerNumberOfPlayersConfiguration {
 
 	@Test
-	public void gameRunnerCharacterizationSixPlayers(){
+	public void gameRunnerCharacterizationSixPlayers() {
 		// given
 		Checker checker = new Checker();
 		System.setOut(new PrintStream(checker));
 		Game aGame = GameRunnerInspector.sixPlayersinit();
 		Random random = new Random(0L);
-		
+
 		// check
 		GameRunner.run(aGame, random);
-		
+
 		// verify
 		assertEquals(2233795382L, checker.getChecksumValue());
 	}
