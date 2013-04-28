5282d7b8619d067b168d75e4097d44f18a67852e
Florentino Romero <fromeroharo@gmail.com>
2013-04-28 23:11:29 +0200

    modificacion clase GameRunner para contemplar test de caracterizacion

--- java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -1,9 +1,8 @@
-
 package com.adaptionsoft.games.trivia.runner;
+
 import java.util.Random;
 
 import com.adaptionsoft.games.uglytrivia.Game;
-
 
 public class GameRunner {
 
@@ -11,15 +10,15 @@ public class GameRunner {
 
 	public static void main(String[] args) {
 		Game aGame = new Game();
-		
+
 		init(aGame);
-		
+
 		Random rand = new Random();
-	
+
 		run(aGame, rand);
-		
+
 	}
-	
+
 	protected static void init(Game aGame) {
 		aGame.add("Chet");
 		aGame.add("Pat");
@@ -28,19 +27,16 @@ public class GameRunner {
 
 	protected static void run(Game aGame, Random rand) {
 		do {
-			
+
 			aGame.roll(rand.nextInt(5) + 1);
-			
+
 			if (rand.nextInt(9) == 7) {
 				notAWinner = aGame.wrongAnswer();
 			} else {
 				notAWinner = aGame.wasCorrectlyAnswered();
 			}
-			
-			
-			
+
 		} while (notAWinner);
 	}
-
 
 }
