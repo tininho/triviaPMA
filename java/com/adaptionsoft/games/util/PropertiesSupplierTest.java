c1731febaee044f0cdb072d1787e5fb489a40cb7
Florentino Romero <fromeroharo@gmail.com>
2013-04-29 22:07:52 +0200

    test para surtidor de propiedades

---
+++ java/com/adaptionsoft/games/util/PropertiesSupplierTest.java
@@ -0,0 +1,14 @@
+package com.adaptionsoft.games.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PropertiesSupplierTest {
+
+	@Test
+	public void obtainOnePropertyMessage() {
+		fail("Not yet implemented");
+	}
+
+}
