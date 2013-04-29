bd8c05c92a39daf30ab9435c59d5fad01969f408
Florentino Romero <fromeroharo@gmail.com>
2013-04-29 21:03:45 +0200

    Implementacion para comprobar que se imprime correctamente al incluir un jugador

--- java/com/adaptionsoft/games/uglytrivia/GameInspectorTest.java
+++ java/com/adaptionsoft/games/uglytrivia/GameInspectorTest.java
@@ -1,14 +1,30 @@
 package com.adaptionsoft.games.uglytrivia;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.io.PrintStream;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import com.adaptionsoft.games.util.Checker;
 
 public class GameInspectorTest {
 
+	Game aGame;
+	Checker checker;
+	
+	@Before
+	public void initilization(){
+		aGame = new GameInspector(6);
+		checker = new Checker();
+		System.setOut(new PrintStream(checker));
+	}
+	
 	@Test
 	public void printMessageAddingPlayers() {
-		fail("Not yet implemented");
+		aGame.add("Romina");
+		assertEquals(535739654L, checker.getChecksumValue());
 	}
 
 }
