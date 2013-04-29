4b0e71a7cad1fedc245837937d911a11a0f51fde
Florentino Romero <fromeroharo@gmail.com>
2013-04-29 20:45:46 +0200

    Creacion de test para clase printer encargada de mostrar mensajes

---
+++ java/com/adaptionsoft/games/util/PrinterTest.java
@@ -0,0 +1,14 @@
+package com.adaptionsoft.games.util;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class PrinterTest {
+
+	@Test
+	public void shouldPrintATextMessage(){
+		fail("not yet implemented");
+	}
+	
+}
