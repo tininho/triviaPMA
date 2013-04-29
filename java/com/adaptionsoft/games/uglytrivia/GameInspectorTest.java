ad83284cc977fb5663ca0fa16429dfc5c615bc3d
Florentino Romero <fromeroharo@gmail.com>
2013-04-29 20:59:39 +0200

    Creacion de test de inspector para comprobar funcionalidad

---
+++ java/com/adaptionsoft/games/uglytrivia/GameInspectorTest.java
@@ -0,0 +1,14 @@
+package com.adaptionsoft.games.uglytrivia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameInspectorTest {
+
+	@Test
+	public void printMessageAddingPlayers() {
+		fail("Not yet implemented");
+	}
+
+}
