7249596e8f56df4a50113c10a8f1c29b785598be
Florentino Romero <fromeroharo@gmail.com>
2013-04-30 09:08:41 +0200

    test para configuracion no permitidano realizado anteriormente

---
+++ java/com/adaptionsoft/games/uglytrivia/GameConfigurationTest.java
@@ -0,0 +1,14 @@
+package com.adaptionsoft.games.uglytrivia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameConfigurationTest {
+
+	@Test
+	public void shouldNotAllowThisConfiguration() {
+		fail("Not yet implemented");
+	}
+
+}
