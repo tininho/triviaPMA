60377d0bc82f349ec88c43dbb409756dc8410b40
Florentino Romero <fromeroharo@gmail.com>
2013-04-30 09:11:09 +0200

    implementacion ok, paso de cobertura

--- java/com/adaptionsoft/games/uglytrivia/GameConfigurationTest.java
+++ java/com/adaptionsoft/games/uglytrivia/GameConfigurationTest.java
@@ -5,10 +5,19 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GameConfigurationTest {
-
+	
 	@Test
 	public void shouldNotAllowThisConfiguration() {
-		fail("Not yet implemented");
+		GameConfiguration configuration = new GameConfiguration();
+		assertEquals(true, configuration.isNotAnAllowedConfiguration(5));
+	}
+	
+	@Test
+	public void shouldAllowThisConfiguration() {
+		GameConfiguration configuration = new GameConfiguration();
+		assertEquals(false, configuration.isNotAnAllowedConfiguration(2));
+		assertEquals(false, configuration.isNotAnAllowedConfiguration(4));
+		assertEquals(false, configuration.isNotAnAllowedConfiguration(6));
 	}
 
 }
