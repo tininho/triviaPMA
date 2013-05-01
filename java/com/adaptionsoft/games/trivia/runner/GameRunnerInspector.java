130c5e103d4010307e53f67f5d1ea01f53fc683f
Florentino Romero <fromeroharo@gmail.com>
2013-05-01 14:02:45 +0200

    inclusion de inspector de gamerunner para comprobar funcionalidad de inicio con idioma

---
+++ java/com/adaptionsoft/games/trivia/runner/GameRunnerInspector.java
@@ -0,0 +1,14 @@
+package com.adaptionsoft.games.trivia.runner;
+
+import java.util.Locale;
+
+import com.adaptionsoft.games.uglytrivia.Game;
+
+public class GameRunnerInspector extends GameRunner{
+
+	public static Game init(int i, Locale locale) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- java/com/adaptionsoft/games/trivia/runner/GameRunnerInspectorTest.java
+++ java/com/adaptionsoft/games/trivia/runner/GameRunnerInspectorTest.java
@@ -1,14 +1,40 @@
 package com.adaptionsoft.games.trivia.runner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.io.PrintStream;
+import java.util.Locale;
+import java.util.Random;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import com.adaptionsoft.games.uglytrivia.Game;
+import com.adaptionsoft.games.util.Checker;
 
 public class GameRunnerInspectorTest {
 
+    Checker checker;
+    GameRunnerInspector runnerInspector;
+	
+	@Before
+	public void initialization(){
+		runnerInspector = new GameRunnerInspector();
+		checker = new Checker();
+		System.setOut(new PrintStream(checker));
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void gameRunnerCharacterizationBySpanishLanguage() {
+		// given
+		Game aGame = GameRunnerInspector.init(6, new Locale("ES"));
+		Random random = new Random(0L);
+
+		// check
+		GameRunner.run(aGame, random);
+
+		// verify
+		assertEquals(1820951815L, checker.getChecksumValue());
 	}
 
 }
