fd5f91e484ca75a8f4d581ee726465a5cf995b96
Florentino Romero <fromeroharo@gmail.com>
2013-05-01 14:33:20 +0200

    pruebas con locale a traves de gameinspector

--- java/com/adaptionsoft/games/trivia/runner/GameRunnerInspector.java
+++ java/com/adaptionsoft/games/trivia/runner/GameRunnerInspector.java
@@ -3,12 +3,18 @@ package com.adaptionsoft.games.trivia.runner;
 import java.util.Locale;
 
 import com.adaptionsoft.games.uglytrivia.Game;
+import com.adaptionsoft.games.uglytrivia.GameInspector;
 
 public class GameRunnerInspector extends GameRunner{
 
-	public static Game init(int i, Locale locale) {
-		// TODO Auto-generated method stub
-		return null;
+	public static Game init(int numberOfPlayers, Locale locale) {
+        Game aGame = new GameInspector(numberOfPlayers, locale);
+		
+		for (int i = 0; i < numberOfPlayers; i++) {
+			aGame.add("Player"+(i+1));
+		}
+		
+		return aGame;
 	}
 
 }
--- java/com/adaptionsoft/games/uglytrivia/GameInspector.java
+++ java/com/adaptionsoft/games/uglytrivia/GameInspector.java
@@ -1,24 +1,41 @@
 package com.adaptionsoft.games.uglytrivia;
 
+import java.util.Locale;
+
 import com.adaptionsoft.games.util.Printer;
 
-public class GameInspector extends Game{
+public class GameInspector extends Game {
 
 	Printer printer;
-	
+	Locale locale;
+
 	public GameInspector(int numberOfPlayers) {
 		super(numberOfPlayers);
 		printer = new Printer();
 	}
 
+	public GameInspector(int numberOfPlayers, Locale locale) {
+		this(numberOfPlayers);
+		this.locale = locale;
+	}
+
 	@Override
-    public boolean add(String playerName) {
-		
-	    players.add(playerName);
-	    
-	    printer.printMessage(playerName, "added");
-	    printer.printMessage("playernumber", players.size());
+	public boolean add(String playerName) {
+
+		players.add(playerName);
+
+		printer.printMessage(playerName, "added");
+		printer.printMessage("playernumber", players.size());
 		return true;
 	}
-	
+
+	public boolean addByLanguage(String playerName) {
+
+		players.add(playerName);
+
+		printer.printMessage(playerName, "added");
+		printer.printMessage("playernumber", players.size());
+		return true;
+	}
+
 }
